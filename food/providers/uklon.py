1b3d82da9297716e656537b59a88068b551cc285
AlexLitvino <litvinov.aleks@gmail.com>
2025-09-27 18:35:03 +0300

    fixed flake8 notifications

--- food/providers/uklon.py
+++ food/providers/uklon.py
@@ -28,8 +28,8 @@ class OrderResponse:
 
 class Client:
     # the url of running service
-    BASE_URL = \
-        f"http://{os.getenv("UKLON_HOST", default="localhost")}:{os.getenv("UKLON_PORT", default="8003")}/drivers/orders"
+    BASE_URL = (f"http://{os.getenv("UKLON_HOST", default="localhost")}:{os.getenv("UKLON_PORT", default="8003")}"
+                f"/drivers/orders")
 
     @classmethod
     def create_order(cls, order: OrderRequestBody):
